cdea377896a73ffc7c93da0fb34b8fd27b5846a6
smonterroza-ai <MS250948@alumno.udb.edu.sv>
2026-05-17 00:57:44 -0600

    mosifique la parte de gestion de unidades

---
+++ AVANCE(PED_GS250179)/Vistas/AgregarUnidad.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace AVANCE_PED_GS250179_
+{
+    partial class AgregarUnidad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AgregarUnidad));
+            Confi_U = new PictureBox();
+            btnAtras = new PictureBox();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            textBox6 = new TextBox();
+            label6 = new Label();
+            ((System.ComponentModel.ISupportInitialize)Confi_U).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnAtras).BeginInit();
+            SuspendLayout();
+            // 
+            // Confi_U
+            // 
+            Confi_U.Cursor = Cursors.Hand;
+            Confi_U.Image = (Image)resources.GetObject("Confi_U.Image");
+            Confi_U.Location = new Point(394, 406);
+            Confi_U.Margin = new Padding(3, 2, 3, 2);
+            Confi_U.Name = "Confi_U";
+            Confi_U.Size = new Size(110, 31);
+            Confi_U.SizeMode = PictureBoxSizeMode.StretchImage;
+            Confi_U.TabIndex = 17;
+            Confi_U.TabStop = false;
+            Confi_U.Click += this.Confi_U_Click;
+            // 
+            // btnAtras
+            // 
+            btnAtras.Cursor = Cursors.Hand;
+            btnAtras.Image = (Image)resources.GetObject("btnAtras.Image");
+            btnAtras.Location = new Point(12, 11);
+            btnAtras.Margin = new Padding(3, 2, 3, 2);
+            btnAtras.Name = "btnAtras";
+            btnAtras.Size = new Size(122, 34);
+            btnAtras.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnAtras.TabIndex = 16;
+            btnAtras.TabStop = false;
+            btnAtras.Click += this.btnAtras_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(304, 155);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 17);
+            label1.TabIndex = 18;
+            label1.Text = "Placa";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(304, 185);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(61, 23);
+            textBox1.TabIndex = 19;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(304, 256);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(135, 23);
+            textBox2.TabIndex = 21;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ControlLightLight;
+            label2.Location = new Point(304, 226);
+            label2.Name = "label2";
+            label2.Size = new Size(72, 17);
+            label2.TabIndex = 20;
+            label2.Text = "Conductor";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(304, 325);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(135, 23);
+            textBox3.TabIndex = 23;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = SystemColors.ControlLightLight;
+            label3.Location = new Point(304, 295);
+            label3.Name = "label3";
+            label3.Size = new Size(45, 17);
+            label3.TabIndex = 22;
+            label3.Text = "Marca";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(459, 185);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(61, 23);
+            textBox4.TabIndex = 25;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.ControlLightLight;
+            label4.Location = new Point(459, 155);
+            label4.Name = "label4";
+            label4.Size = new Size(36, 17);
+            label4.TabIndex = 24;
+            label4.Text = "Ruta";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(459, 256);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(135, 23);
+            textBox5.TabIndex = 27;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = SystemColors.ControlLightLight;
+            label5.Location = new Point(459, 226);
+            label5.Name = "label5";
+            label5.Size = new Size(49, 17);
+            label5.TabIndex = 26;
+            label5.Text = "Estado";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(459, 325);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(135, 23);
+            textBox6.TabIndex = 29;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = SystemColors.ControlLightLight;
+            label6.Location = new Point(459, 295);
+            label6.Name = "label6";
+            label6.Size = new Size(55, 17);
+            label6.TabIndex = 28;
+            label6.Text = "Modelo";
+            // 
+            // AgregarUnidad
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.fondo2_Agregar_;
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(897, 488);
+            Controls.Add(textBox6);
+            Controls.Add(label6);
+            Controls.Add(textBox5);
+            Controls.Add(label5);
+            Controls.Add(textBox4);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(Confi_U);
+            Controls.Add(btnAtras);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "AgregarUnidad";
+            Text = "Agregar unidad";
+            ((System.ComponentModel.ISupportInitialize)Confi_U).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnAtras).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox Confi_U;
+        private PictureBox btnAtras;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox4;
+        private Label label4;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox6;
+        private Label label6;
+    }
+}
--- AVANCE(PED_GS250179)/Vistas/Form2.cs
+++ AVANCE(PED_GS250179)/Vistas/Form2.cs
@@ -73,7 +73,11 @@ namespace AVANCE_PED_GS250179_
         private void btnU_Click(object sender, EventArgs e)
         {
             GestionUnidades Uni = new GestionUnidades();
+<<<<<<< Updated upstream
             Uni.Show(this);
+=======
+            Uni.Show();
+>>>>>>> Stashed changes
 
             this.Hide();
         }
