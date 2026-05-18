5b5400db2bfacbcc351b0836e1d097fdc4c2bf45
smonterroza-ai <MS250948@alumno.udb.edu.sv>
2026-05-17 21:40:06 -0600

    ya funciona agregar en gestionunidades

--- AVANCE(PED_GS250179)/Program.cs
+++ AVANCE(PED_GS250179)/Program.cs
@@ -11,7 +11,7 @@ namespace AVANCE_PED_GS250179_
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form6());
         }
     }
 }
