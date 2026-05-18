f3d0beb806fc99c86bf34cc507876f1aa979e428
Elmerrf56 <elmerreales36@gmail.com>
2026-05-17 22:12:20 -0600

    forms

---
+++ AVANCE(PED_GS250179)/Vistas/Form11.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace AVANCE_PED_GS250179_.Vistas
+{
+    partial class Form11
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form11));
+            txtBuscar = new TextBox();
+            dgvRutas = new DataGridView();
+            label6 = new Label();
+            btnEliminar = new PictureBox();
+            btnEditarM = new PictureBox();
+            btnAR = new PictureBox();
+            btnAtras = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)dgvRutas).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnEliminar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnEditarM).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnAR).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnAtras).BeginInit();
+            SuspendLayout();
+            // 
+            // txtBuscar
+            // 
+            txtBuscar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtBuscar.ForeColor = SystemColors.WindowText;
+            txtBuscar.Location = new Point(710, 95);
+            txtBuscar.Margin = new Padding(3, 4, 3, 4);
+            txtBuscar.Name = "txtBuscar";
+            txtBuscar.PlaceholderText = "Buscar por placa";
+            txtBuscar.Size = new Size(282, 29);
+            txtBuscar.TabIndex = 28;
+            // 
+            // dgvRutas
+            // 
+            dgvRutas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvRutas.Location = new Point(37, 136);
+            dgvRutas.Name = "dgvRutas";
+            dgvRutas.RowHeadersWidth = 51;
+            dgvRutas.Size = new Size(955, 301);
+            dgvRutas.TabIndex = 27;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.FromArgb(0, 0, 64);
+            label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.ImageAlign = ContentAlignment.BottomCenter;
+            label6.Location = new Point(37, 87);
+            label6.Name = "label6";
+            label6.Size = new Size(545, 37);
+            label6.TabIndex = 26;
+            label6.Text = "GESTION DE RUTAS";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Cursor = Cursors.Hand;
+            btnEliminar.Image = (Image)resources.GetObject("btnEliminar.Image");
+            btnEliminar.Location = new Point(910, 14);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(126, 40);
+            btnEliminar.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnEliminar.TabIndex = 25;
+            btnEliminar.TabStop = false;
+            // 
+            // btnEditarM
+            // 
+            btnEditarM.Cursor = Cursors.Hand;
+            btnEditarM.Image = (Image)resources.GetObject("btnEditarM.Image");
+            btnEditarM.Location = new Point(764, 14);
+            btnEditarM.Name = "btnEditarM";
+            btnEditarM.Size = new Size(126, 40);
+            btnEditarM.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnEditarM.TabIndex = 24;
+            btnEditarM.TabStop = false;
+            // 
+            // btnAR
+            // 
+            btnAR.Cursor = Cursors.Hand;
+            btnAR.Image = (Image)resources.GetObject("btnAR.Image");
+            btnAR.Location = new Point(620, 14);
+            btnAR.Name = "btnAR";
+            btnAR.Size = new Size(126, 40);
+            btnAR.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnAR.TabIndex = 23;
+            btnAR.TabStop = false;
+            // 
+            // btnAtras
+            // 
+            btnAtras.Cursor = Cursors.Hand;
+            btnAtras.Image = (Image)resources.GetObject("btnAtras.Image");
+            btnAtras.Location = new Point(-11, 513);
+            btnAtras.Name = "btnAtras";
+            btnAtras.Size = new Size(139, 45);
+            btnAtras.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnAtras.TabIndex = 22;
+            btnAtras.TabStop = false;
+            // 
+            // Form11
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.fondo1;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1057, 573);
+            Controls.Add(txtBuscar);
+            Controls.Add(dgvRutas);
+            Controls.Add(label6);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnEditarM);
+            Controls.Add(btnAR);
+            Controls.Add(btnAtras);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form11";
+            Text = "Form11";
+            ((System.ComponentModel.ISupportInitialize)dgvRutas).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnEliminar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnEditarM).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnAR).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnAtras).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtBuscar;
+        private DataGridView dgvRutas;
+        private Label label6;
+        private PictureBox btnEliminar;
+        private PictureBox btnEditarM;
+        private PictureBox btnAR;
+        private PictureBox btnAtras;
+    }
+}
