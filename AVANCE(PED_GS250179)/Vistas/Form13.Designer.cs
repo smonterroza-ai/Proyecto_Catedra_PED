3c672b98afaaf7f3355c016a5b2a91f493f8f54c
Elmerrf56 <elmerreales36@gmail.com>
2026-05-18 21:10:16 -0600

    diseño

---
+++ AVANCE(PED_GS250179)/Vistas/Form13.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace AVANCE_PED_GS250179_.Vistas
+{
+    partial class Form13
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRegresar = new Button();
+            txtBuscar = new TextBox();
+            dgvTransaccion = new DataGridView();
+            label6 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dgvTransaccion).BeginInit();
+            SuspendLayout();
+            // 
+            // btnRegresar
+            // 
+            btnRegresar.BackColor = Color.DarkGray;
+            btnRegresar.FlatAppearance.BorderSize = 0;
+            btnRegresar.FlatStyle = FlatStyle.Flat;
+            btnRegresar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRegresar.ForeColor = SystemColors.ControlLightLight;
+            btnRegresar.Image = Properties.Resources.esquema_de_boton_circular_de_flecha_hacia_atras_izquierda;
+            btnRegresar.Location = new Point(370, 567);
+            btnRegresar.Margin = new Padding(3, 4, 3, 4);
+            btnRegresar.Name = "btnRegresar";
+            btnRegresar.Size = new Size(127, 51);
+            btnRegresar.TabIndex = 34;
+            btnRegresar.Text = "  Regresar";
+            btnRegresar.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnRegresar.UseVisualStyleBackColor = false;
+            // 
+            // txtBuscar
+            // 
+            txtBuscar.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            txtBuscar.ForeColor = SystemColors.WindowText;
+            txtBuscar.Location = new Point(750, 36);
+            txtBuscar.Margin = new Padding(3, 4, 3, 4);
+            txtBuscar.Name = "txtBuscar";
+            txtBuscar.PlaceholderText = "Buscar por transacción";
+            txtBuscar.Size = new Size(244, 29);
+            txtBuscar.TabIndex = 33;
+            // 
+            // dgvTransaccion
+            // 
+            dgvTransaccion.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvTransaccion.Location = new Point(370, 92);
+            dgvTransaccion.Name = "dgvTransaccion";
+            dgvTransaccion.RowHeadersWidth = 51;
+            dgvTransaccion.Size = new Size(625, 449);
+            dgvTransaccion.TabIndex = 32;
+            // 
+            // label6
+            // 
+            label6.BackColor = Color.FromArgb(0, 0, 64);
+            label6.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.ImageAlign = ContentAlignment.BottomCenter;
+            label6.Location = new Point(370, 29);
+            label6.Name = "label6";
+            label6.Size = new Size(337, 37);
+            label6.TabIndex = 31;
+            label6.Text = "MONITOR DE TRANSACCIONES";
+            label6.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // Form13
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.fondo_transacciones;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(1007, 627);
+            Controls.Add(btnRegresar);
+            Controls.Add(txtBuscar);
+            Controls.Add(dgvTransaccion);
+            Controls.Add(label6);
+            DoubleBuffered = true;
+            Name = "Form13";
+            Text = "Form13";
+            ((System.ComponentModel.ISupportInitialize)dgvTransaccion).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnRegresar;
+        private TextBox txtBuscar;
+        private DataGridView dgvTransaccion;
+        private Label label6;
+    }
+}
--- AVANCE(PED_GS250179)/Vistas/Form9.Designer.cs
+++ AVANCE(PED_GS250179)/Vistas/Form9.Designer.cs
@@ -55,9 +55,10 @@
             // 
             // txtNombre
             // 
-            txtNombre.Location = new Point(165, 109);
+            txtNombre.Location = new Point(189, 145);
+            txtNombre.Margin = new Padding(3, 4, 3, 4);
             txtNombre.Name = "txtNombre";
-            txtNombre.Size = new Size(418, 23);
+            txtNombre.Size = new Size(477, 27);
             txtNombre.TabIndex = 0;
             txtNombre.KeyPress += txtNombre_KeyPress;
             // 
@@ -67,9 +68,9 @@
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = SystemColors.ControlLightLight;
-            label1.Location = new Point(165, 76);
+            label1.Location = new Point(189, 101);
             label1.Name = "label1";
-            label1.Size = new Size(58, 17);
+            label1.Size = new Size(76, 23);
             label1.TabIndex = 1;
             label1.Text = "Nombre";
             // 
@@ -79,17 +80,19 @@
             label2.BackColor = Color.Transparent;
             label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label2.ForeColor = SystemColors.ControlLightLight;
-            label2.Location = new Point(165, 148);
+            label2.Location = new Point(189, 197);
             label2.Name = "label2";
-            label2.Size = new Size(31, 17);
+            label2.Size = new Size(40, 23);
             label2.TabIndex = 3;
             label2.Text = "DUI";
             // 
             // txtDui
             // 
-            txtDui.Location = new Point(165, 181);
+            txtDui.Location = new Point(189, 241);
+            txtDui.Margin = new Padding(3, 4, 3, 4);
+            txtDui.MaxLength = 10;
             txtDui.Name = "txtDui";
-            txtDui.Size = new Size(196, 23);
+            txtDui.Size = new Size(223, 27);
             txtDui.TabIndex = 2;
             txtDui.KeyPress += txtDui_KeyPress;
             // 
@@ -99,17 +102,18 @@
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label3.ForeColor = SystemColors.ControlLightLight;
-            label3.Location = new Point(163, 222);
+            label3.Location = new Point(186, 296);
             label3.Name = "label3";
-            label3.Size = new Size(49, 17);
+            label3.Size = new Size(64, 23);
             label3.TabIndex = 5;
             label3.Text = "Correo";
             // 
             // txtCorreo
             // 
-            txtCorreo.Location = new Point(163, 255);
+            txtCorreo.Location = new Point(186, 340);
+            txtCorreo.Margin = new Padding(3, 4, 3, 4);
             txtCorreo.Name = "txtCorreo";
-            txtCorreo.Size = new Size(259, 23);
+            txtCorreo.Size = new Size(295, 27);
             txtCorreo.TabIndex = 4;
             // 
             // label4
@@ -118,17 +122,18 @@
             label4.BackColor = Color.Transparent;
             label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label4.ForeColor = SystemColors.ControlLightLight;
-            label4.Location = new Point(157, 293);
+            label4.Location = new Point(179, 391);
             label4.Name = "label4";
-            label4.Size = new Size(55, 17);
+            label4.Size = new Size(70, 23);
             label4.TabIndex = 7;
             label4.Text = "Usuario";
             // 
             // txtUsuario
             // 
-            txtUsuario.Location = new Point(157, 326);
+            txtUsuario.Location = new Point(179, 435);
+            txtUsuario.Margin = new Padding(3, 4, 3, 4);
             txtUsuario.Name = "txtUsuario";
-            txtUsuario.Size = new Size(198, 23);
+            txtUsuario.Size = new Size(226, 27);
             txtUsuario.TabIndex = 6;
             // 
             // label5
@@ -137,17 +142,18 @@
             label5.BackColor = Color.Transparent;
             label5.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label5.ForeColor = SystemColors.ControlLightLight;
-            label5.Location = new Point(367, 148);
+            label5.Location = new Point(419, 197);
             label5.Name = "label5";
-            label5.Size = new Size(135, 17);
+            label5.Size = new Size(174, 23);
             label5.TabIndex = 8;
             label5.Text = "Fecha de nacimiento";
             // 
             // dtpNa
             // 
-            dtpNa.Location = new Point(367, 181);
+            dtpNa.Location = new Point(419, 241);
+            dtpNa.Margin = new Padding(3, 4, 3, 4);
             dtpNa.Name = "dtpNa";
-            dtpNa.Size = new Size(216, 23);
+            dtpNa.Size = new Size(246, 27);
             dtpNa.TabIndex = 9;
             // 
             // label6
@@ -156,26 +162,29 @@
             label6.BackColor = Color.Transparent;
             label6.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label6.ForeColor = SystemColors.ControlLightLight;
-            label6.Location = new Point(428, 222);
+            label6.Location = new Point(489, 296);
             label6.Name = "label6";
-            label6.Size = new Size(62, 17);
+            label6.Size = new Size(78, 23);
             label6.TabIndex = 11;
             label6.Text = "Télefono";
             // 
             // txtTel
             // 
-            txtTel.Location = new Point(428, 255);
+            txtTel.Location = new Point(489, 340);
+            txtTel.Margin = new Padding(3, 4, 3, 4);
+            txtTel.MaxLength = 9;
             txtTel.Name = "txtTel";
-            txtTel.Size = new Size(155, 23);
+            txtTel.Size = new Size(177, 27);
             txtTel.TabIndex = 10;
             txtTel.KeyPress += txtTel_KeyPress;
             // 
             // txtPass
             // 
-            txtPass.Location = new Point(157, 404);
+            txtPass.Location = new Point(179, 539);
+            txtPass.Margin = new Padding(3, 4, 3, 4);
             txtPass.Name = "txtPass";
             txtPass.PasswordChar = '*';
-            txtPass.Size = new Size(198, 23);
+            txtPass.Size = new Size(226, 27);
             txtPass.TabIndex = 12;
             // 
             // label7
@@ -184,17 +193,18 @@
             label7.BackColor = Color.Transparent;
             label7.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label7.ForeColor = SystemColors.ControlLightLight;
-            label7.Location = new Point(385, 363);
+            label7.Location = new Point(440, 484);
             label7.Name = "label7";
-            label7.Size = new Size(141, 17);
+            label7.Size = new Size(183, 23);
             label7.TabIndex = 13;
             label7.Text = "Confirmar contraseña";
             // 
             // dtpCon
             // 
-            dtpCon.Location = new Point(367, 326);
+            dtpCon.Location = new Point(419, 435);
+            dtpCon.Margin = new Padding(3, 4, 3, 4);
             dtpCon.Name = "dtpCon";
-            dtpCon.Size = new Size(216, 23);
+            dtpCon.Size = new Size(246, 27);
             dtpCon.TabIndex = 15;
             // 
             // label8
@@ -203,9 +213,9 @@
             label8.BackColor = Color.Transparent;
             label8.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label8.ForeColor = SystemColors.ControlLightLight;
-            label8.Location = new Point(367, 293);
+            label8.Location = new Point(419, 391);
             label8.Name = "label8";
-            label8.Size = new Size(143, 17);
+            label8.Size = new Size(185, 23);
             label8.TabIndex = 14;
             label8.Text = "Fecha de contratación";
             // 
@@ -215,28 +225,28 @@
             label9.BackColor = Color.Transparent;
             label9.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label9.ForeColor = SystemColors.ControlLightLight;
-            label9.Location = new Point(157, 363);
+            label9.Location = new Point(179, 484);
             label9.Name = "label9";
-            label9.Size = new Size(77, 17);
+            label9.Size = new Size(99, 23);
             label9.TabIndex = 16;
             label9.Text = "Contraseña";
             label9.Click += label9_Click;
             // 
             // txtConfPass
             // 
-            txtConfPass.Location = new Point(385, 404);
+            txtConfPass.Location = new Point(440, 539);
+            txtConfPass.Margin = new Padding(3, 4, 3, 4);
             txtConfPass.Name = "txtConfPass";
             txtConfPass.PasswordChar = '*';
-            txtConfPass.Size = new Size(198, 23);
+            txtConfPass.Size = new Size(226, 27);
             txtConfPass.TabIndex = 19;
             // 
             // ptojo
             // 
             ptojo.Image = Properties.Resources.icons8_eye_24;
-            ptojo.Location = new Point(328, 404);
-            ptojo.Margin = new Padding(3, 2, 3, 2);
+            ptojo.Location = new Point(375, 539);
             ptojo.Name = "ptojo";
-            ptojo.Size = new Size(27, 20);
+            ptojo.Size = new Size(31, 27);
             ptojo.TabIndex = 20;
             ptojo.TabStop = false;
             ptojo.Click += ptojo_Click;
@@ -244,10 +254,9 @@
             // ptojo2
             // 
             ptojo2.Image = Properties.Resources.icons8_eye_24;
-            ptojo2.Location = new Point(556, 404);
-            ptojo2.Margin = new Padding(3, 2, 3, 2);
+            ptojo2.Location = new Point(635, 539);
             ptojo2.Name = "ptojo2";
-            ptojo2.Size = new Size(27, 20);
+            ptojo2.Size = new Size(31, 27);
             ptojo2.TabIndex = 21;
             ptojo2.TabStop = false;
             ptojo2.Click += ptojo2_Click;
@@ -260,9 +269,10 @@
             button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.ForeColor = SystemColors.ControlLightLight;
             button1.Image = Properties.Resources.marca_de_verificacion;
-            button1.Location = new Point(321, 463);
+            button1.Location = new Point(367, 617);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(101, 38);
+            button1.Size = new Size(115, 51);
             button1.TabIndex = 45;
             button1.Text = "  Listo";
             button1.TextImageRelation = TextImageRelation.ImageBeforeText;
@@ -271,11 +281,11 @@
             // 
             // Form9
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.fondo_primeruso;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(730, 530);
+            ClientSize = new Size(834, 707);
             Controls.Add(button1);
             Controls.Add(ptojo2);
             Controls.Add(ptojo);
@@ -299,7 +309,6 @@
             Controls.Add(txtNombre);
             DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form9";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form9";
